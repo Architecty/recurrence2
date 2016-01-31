a2a3a1465aa03e534f64d58c2e9210c57e20c5e3
Daniel C <danclayson@gmail.com>
2016-01-31 11:23:28 -0800

    Menu button works

---
+++ Assets/clickToStart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickToStart : MonoBehaviour {
+
+    public Material hoverMaterial;
+    public Material notHoverMaterial;
+    public Transform redButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        int i = Application.loadedLevel;
+        Application.LoadLevel(i + 1);
+    }
+    void OnMouseEnter()
+    {
+        redButton.GetComponent<Renderer>().material = hoverMaterial;
+    }
+    void OnMouseExit()
+    {
+        redButton.GetComponent<Renderer>().material = notHoverMaterial;
+    }
+}
